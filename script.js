751099c66991ffa01b3447c8d4c309fc75473250
João Gonçalves <joaopedro.jp.4998@gmail.com>
2022-11-04 18:03:46 +0000

    Create helper function to set attributes on DOM elements

--- script.js
+++ script.js
@@ -18,18 +18,33 @@ const getPhotos = async () => {
   } catch (error) {}
 };
 
+// Helper functio to set atrtributes on DOM elements
+const setAttributes = (element, attributes) => {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+};
+
 // Create element that will show photos
 const displayPhotos = () => {
   photosArray.forEach((photo) => {
     const imgBox = document.createElement("div");
 
     const item = document.createElement("a");
-    item.setAttribute("href", photo.links.html);
-    item.setAttribute("target", "blank");
+    // item.setAttribute("href", photo.links.html);
+    // item.setAttribute("target", "blank");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
 
     const img = document.createElement("img");
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+    });
 
     const button = document.createElement("button");
     button.textContent = "download me";
