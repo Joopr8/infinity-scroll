3651fcdc03e7e1f55140b64a890eb760b7f045a2
João Gonçalves <joaopedro.jp.4998@gmail.com>
2022-11-04 18:43:29 +0000

    Add infinty scroll

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
 const imageContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = "UC4fRvsNFHRQZgBNiPCBLECPu9dyQC18KpH5MytsTmw";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//Check if the images is loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+  }
+}
 
 // Get photos
 const getPhotos = async () => {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
-    console.log(photosArray);
     displayPhotos();
   } catch (error) {}
 };
 
-// Helper functio to set atrtributes on DOM elements
+// Helper functio to set attributes on DOM elements
 const setAttributes = (element, attributes) => {
   for (const key in attributes) {
     element.setAttribute(key, attributes[key]);
@@ -27,6 +38,7 @@ const setAttributes = (element, attributes) => {
 
 // Create element that will show photos
 const displayPhotos = () => {
+  totalImages = photosArray.length;
   photosArray.forEach((photo) => {
     const imgBox = document.createElement("div");
 
@@ -44,17 +56,32 @@ const displayPhotos = () => {
     setAttributes(img, {
       src: photo.urls.regular,
       alt: photo.alt_description,
+      title: photo.alt_description,
     });
 
-    const button = document.createElement("button");
-    button.textContent = "download me";
-    //button.setAttribute("onclick", window.open(photo.links.download));
+    img.addEventListener("load", imageLoaded);
+
+    // const button = document.createElement("button");
+    // button.textContent = "download me";
+    // //button.setAttribute("onclick", window.open(photo.links.download));
 
     item.appendChild(img);
     imgBox.appendChild(item);
-    imgBox.appendChild(button);
+    //imgBox.appendChild(button);
     imageContainer.appendChild(imgBox);
   });
 };
+
+//Check to see if scrolling near bottom of page, load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    imagesLoaded = 0;
+    getPhotos();
+  }
+});
 
 getPhotos();
